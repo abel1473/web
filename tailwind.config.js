dd91feaa3bc418b10a150394e6206022dd4eb3ac
abel1473 <161975951+abel1473@users.noreply.github.com>
2025-01-17 13:57:03 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        'node_modules/preline/dist/*.js',
+      ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [
+        require('preline/plugin'),
+    ],
+};
